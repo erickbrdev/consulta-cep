7a514293c4cd21fc2b997a4629da971c3be8a6f1
Erick-Andrade-Dev <erick.dev07@gmail.com>
2022-12-24 13:26:59 -0300

    navegacao entre telas

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
+import { useState } from 'react';
 import './App.css';
 import Pesquisa from './Components/Pesquisa';
 import Carregando from './Components/Carregando';
 import Resultados from './Components/Resultados';
 import Error from './Components/Error';
 
-function App() {  
+function App() { 
+  const [numTela, setNumTela] = useState(0);
+
+ 
+  function handleClick() {
+    setNumTela((numTela + 1) % 4)   
+  }
+  
   return (
     <div>
-      <button>Prox tela</button>
-      <Resultados result={{
-        Rua: "Prado",
-        Cidade: "Via Lagos",  
-        Estado: "RJ"   
-      }}/>
+      <button onClick={() => handleClick()}>Prox tela</button>
+      {numTela === 0 && <Pesquisa/>}
+      {numTela === 1 && <Resultados result= {{
+        Rua: 'Prado'
+      }}/>}
+      {numTela === 2 && <Error errorMessage="CEP não localizado"/>}
+      {numTela === 3 && <Carregando/>}
     </div>
   );
 }
